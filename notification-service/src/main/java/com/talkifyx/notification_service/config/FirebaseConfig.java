73507659d217879c4dd9e5972e3ac092515dea00
SumitSaini1 <sumitsaini21027@gmail.com>
2026-05-07 23:06:56 +0530

    [Sumit Saini] Fix: Firebase config for Docker deployment

--- notification-service/src/main/java/com/talkifyx/notification_service/config/FirebaseConfig.java
+++ notification-service/src/main/java/com/talkifyx/notification_service/config/FirebaseConfig.java
@@ -3,26 +3,33 @@ package com.talkifyx.notification_service.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Configuration;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_CONFIG_PATH}")
+    private String firebaseConfigPath;
+
     @PostConstruct
     public void initialize() throws IOException {
+
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount =
-                    getClass().getClassLoader().getResourceAsStream("firebase-service-account.json");
-            if (serviceAccount == null) {
-                throw new IllegalStateException("firebase-service-account.json not found in resources");
-            }
+
+            FileInputStream serviceAccount =
+                    new FileInputStream(firebaseConfigPath);
+
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
+
             FirebaseApp.initializeApp(options);
+
             System.out.println("Firebase Initialized Successfully");
         }
     }
